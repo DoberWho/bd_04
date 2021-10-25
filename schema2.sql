142b0ea71d92e05ac45091ffb9c71f837a8d39b7
Jorge Parada <jorge.parada@ctbarbanza.com>
2021-10-25 13:03:21 +0200

    Consultas Prueba

---
+++ schema2.sql
@@ -0,0 +1,7 @@
+select * from sustantivos where id_adjetivo = 1;
+
+
+select * from sustantivos where id_adjetivo = 1 AND nombre = "Jose";
+
+
+select * from sustantivos where id_adjetivo = 1 AND nombre LIKE "%Jose%";
