1e1ebe44e715d2d20351f0c8c4ce3a15bb7fcd2a
Jorge Parada <jorge.parada@ctbarbanza.com>
2021-11-22 10:18:21 +0100

    -

--- movies.db.sql
+++ movies.db.sql
@@ -6,7 +6,30 @@ ORDER BY f.film_id DESC, nombre ASC
 LIMIT 5 OFFSET 10;
 
 
+select COUNT(amount) as cantidad, SUM(amount) as total,
+AVG(amount) as promedio
+from payment
+where customer_id = 201;
 
+
+SELECT customer_id, SUM(amount) as cantidad
+from payment
+GROUP BY customer_id;
+
+
+select c.customer_id, c.first_name, c.last_name,
+SUM(p.amount) as total, AVG(p.amount) as promedio,
+MAX(p.payment_date) as ultima_compra,
+MIN(p.payment_date) as primera_compra
+from customer c
+join payment as p on c.customer_id = p.customer_id
+GROUP BY p.customer_id;
+
+
+SELECT MAX(payment_date) as max,
+MIN(payment_date) as min,
+AVG(payment_date) as promedio
+from payment;
 
 SELECT f.film_id as id, f.title, UPPER(la.name)
 FROM film as f
